d9e2780fb5082c4d0abb1fed824578c39b14dc33
JiwonChung <jimmychung00@naver.com>
2021-02-23 20:04:48 +0900

    .

--- CalCulatePlayground/LinearRegression/cal.java
+++ CalCulatePlayground/LinearRegression/cal.java
@@ -1,39 +1,42 @@
-
 public class cal {
     public static void main(String[] args) {
-        int[] xs = { 1, 2, 3 };
-        int[] ys = { 2, 4, 6 };
+        int[] xs = {1, 2, 3};
+        int[] ys = {2, 3, 4};
 
-        double weight = 6.75;
-        double basis = 0;
-        // System.out.println(returnDerivative(xs, ys, weight, basis));
+        double weight = 0;
+        double basis = 1;
 
+        System.out.println("inclination : " + returnDifferentialCoefficient(xs, ys, weight, basis));
 
-        for (int i = 0; i < 50000; i++) {
-            double inclination = returnDerivative(xs, ys, weight, basis);
-            if (inclination == 0) {
-                break;
-            } else if (inclination < 0) {
+        double tmp = 0;
+        for (int i = 1; i < 10001; i++) {
+            double inclination = returnDifferentialCoefficient(xs, ys, weight, basis);
+            tmp = inclination;
+            if (inclination < 0) {
                 weight += 0.001;
             } else {
                 weight -= 0.001;
             }
-
-            if (i % 200 == 0) {
+            if (i % 1000 == 0) {
                 System.out.println(i + " times weight : " + weight);
             }
         }
-
-        System.out.println(weight);
-
+        System.out.println("inclination : " + tmp);
+        
     }
 
-    static double returnDerivative(int[] xs, int[] ys, double weight, double basis) {
-        int m = 3;
-        double derivativesInclination = 0;
+    // H(x) = x * weight + basis
+    // cost(weight, basis) = sigma(xs.length - 1, i = 0)[H(xs[i]) - ys[i]] / xs.length
+    // (Derivative)cost(weight, basis) = sigma(xs.length - 1, i = 0)[2 * xs[i] + 2 * xs[i] * (basis - ys[i])]
+    //       weight
+    static double returnDifferentialCoefficient(int[] xs, int[] ys, double weight, double basis) {
+        int m = xs.length;
+        double tangentLinesInclination = 0;
+
+        // 도함수
         for (int i = 0; i < m; i++) {
-            derivativesInclination += 2.0 * Math.sqrt(xs[i]) * weight + 2.0 * xs[i] * (basis - ys[i]);
+            tangentLinesInclination += 2 * Math.sqrt(xs[i]) * weight + 2 * xs[i] * (basis - ys[i]);
         }
-        return derivativesInclination / m;
+        return tangentLinesInclination / m;
     }
 }
