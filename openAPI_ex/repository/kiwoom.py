991527dfe89d2a63ca5a8c95b71c76dfd7e3c4d4
JiwonChung <jimmychung00@naver.com>
2021-05-08 17:46:45 +0900

    나도 모르는세 피어나 그건 아마 우리의 잘못은 아닐 거야 그러니 우린 손을 잡아야해

--- openAPI_ex/repository/kiwoom.py
+++
@@ -1,3 +0,0 @@
-class KiwoomRepository:
-    def __init__(self):
-        print("키움 리포지토리 입니다. ")
--- openAPI_ex/view/ui.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import *
-import sys
-
-
-class UI:
-    def __init__(self):
-        self.app = QApplication(sys.argv)
-        print("ui 클래스 입니다. ")
-        self.app.exec()
